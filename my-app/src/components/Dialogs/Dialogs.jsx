f5f12dd617c1d5d2376762c2a0393b93840bd956
Mariy <marodina87@gmail.com>
2020-03-26 22:54:08 +0300

    add style to dialog

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const  Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.item + ' ' + classes.active}>
+                    <NavLink to='dialogs/1'>
+                        Andrey
+                    </NavLink>
+                </div>
+                <div className={classes.item}>
+                    <NavLink to='dialogs/2'>
+                        Sergey
+                    </NavLink>
+                </div>
+                <div className={classes.item}>Anna</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.messagesItem}>Hi</div>
+                <div className={classes.messagesItem}>How are you?</div>
+                <div className={classes.messagesItem}>Hello!</div>
+            </div>
         </div>
     )
 }
